1780a900e633ea9d847d87d862ad811b5bbb6492
jiandeng <jiandeng.develop@gmail.com>
2017-09-16 10:36:55 +0800

    Clean up test cases

--- source/main.cpp
+++ source/main.cpp
@@ -10,53 +10,332 @@
     complete the operation loop.
 
     */
-#define ITERATIONS  1000000                         // Number of calculations.
+#define ITERATIONS  100000                         // Number of calculations.
 #define CLOCK       (SystemCoreClock/1000000.0f)    // Clock freqency in MHz
 Timer timer;                        // Timer..
 
 Serial pc(USBTX, USBRX);
-float number_of_cycles, single_operation_time;
+float operation_cycles, operation_time;
 volatile float a, b,c;            // Float operands and result. Must be volatile!
 //volatile int a, b,c;              // Int operands and result. Must be volatile!
 
-int main() {
+int test_for(int iterations) {
+    int total_time;
+    volatile int i;
 
-    unsigned int i, for_time, total_time, operation_time;
-    a=2.3;
-    b=5.33;
-
-    pc.printf("Operations in progress.. May take some time.\n\n");
-    timer.reset();      // Reset timer
-    timer.start();      // Start timer
+    pc.printf("Testing for()...");
     /* Determine loop overhead */
-    for (i=0; i<ITERATIONS; i++);
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+    }
+    total_time=timer.read_us();
+    timer.stop();
+
+    printf("\tTotalTime=%dus, AverageTime=%.3fus (%.3fcycles)\n", total_time, total_time/float(iterations), total_time/float(iterations)*CLOCK);
+    return total_time;
+}
+
+int test_plus(int iterations) {
+    int for_time, total_time;
+    volatile float a, b, c;
+    volatile int i;
+
+    pc.printf("Testing (+)...");
+    /* Determine loop overhead */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+    }
+    for_time=timer.read_us();
+    timer.stop();
+
+    a = 1.7;
+    b = 2.2;
+    /* Determine the total loop time */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+        c=a+b;
+    }
+    total_time=timer.read_us();
+    timer.stop();
+
+    total_time -= for_time;
+    printf("\tTotalTime=%dus, AverageTime=%.3fus (%.3fcycles)\n", total_time, total_time/float(iterations), total_time/float(iterations)*CLOCK);
+
+    return total_time;
+}
+
+int test_sub(int iterations) {
+    int for_time, total_time;
+    volatile float a, b, c;
+    volatile int i;
+
+    pc.printf("Testing (-)...");
+    /* Determine loop overhead */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+    }
+    for_time=timer.read_us();
+    timer.stop();
+
+    a = 1.7;
+    b = 2.2;
+    /* Determine the total loop time */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+        c=a-b;
+    }
+    total_time=timer.read_us();
+    timer.stop();
+
+    total_time -= for_time;
+    printf("\tTotalTime=%dus, AverageTime=%.3fus (%.3fcycles)\n", total_time, total_time/float(iterations), total_time/float(iterations)*CLOCK);
+
+    return total_time;
+}
+
+int test_mul(int iterations) {
+    int for_time, total_time;
+    volatile float a, b, c;
+    volatile int i;
+
+    pc.printf("Testing (*)...");
+    /* Determine loop overhead */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+    }
+    for_time=timer.read_us();
+    timer.stop();
+
+    a = 1.7;
+    b = 2.2;
+    /* Determine the total loop time */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+        c=a*b;
+    }
+    total_time=timer.read_us();
+    timer.stop();
+
+    total_time -= for_time;
+    printf("\tTotalTime=%dus, AverageTime=%.3fus (%.3fcycles)\n", total_time, total_time/float(iterations), total_time/float(iterations)*CLOCK);
+
+    return total_time;
+}
+
+int test_div(int iterations) {
+    int for_time, total_time;
+    volatile float a, b, c;
+    volatile int i;
+
+    pc.printf("Testing (/)...");
+    /* Determine loop overhead */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+    }
+    for_time=timer.read_us();
+    timer.stop();
+
+    a = 1.7;
+    b = 2.2;
+    /* Determine the total loop time */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+        c=a/b;
+    }
+    total_time=timer.read_us();
+    timer.stop();
+
+    total_time -= for_time;
+    printf("\tTotalTime=%dus, AverageTime=%.3fus (%.3fcycles)\n", total_time, total_time/float(iterations), total_time/float(iterations)*CLOCK);
+
+    return total_time;
+}
+
+int test_sin(int iterations) {
+    int for_time, total_time;
+    volatile float a, b, c;
+    volatile int i;
+
+    pc.printf("Testing sin()...");
+    /* Determine loop overhead */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+    }
+    for_time=timer.read_us();
+    timer.stop();
+
+    a = 1.7;
+    b = 2.2;
+    /* Determine the total loop time */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+        c=sin(a);
+    }
+    total_time=timer.read_us();
+    timer.stop();
+
+    total_time -= for_time;
+    printf("\tTotalTime=%dus, AverageTime=%.3fus (%.3fcycles)\n", total_time, total_time/float(iterations), total_time/float(iterations)*CLOCK);
+
+    return total_time;
+}
+
+int test_sqrt(int iterations) {
+    int for_time, total_time;
+    volatile float a, b, c;
+    volatile int i;
+
+    pc.printf("Testing sqrt()...");
+    /* Determine loop overhead */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+    }
+    for_time=timer.read_us();
+    timer.stop();
+
+    a = 1.7;
+    b = 2.2;
+    /* Determine the total loop time */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+        c=sqrt(a);
+    }
+    total_time=timer.read_us();
+    timer.stop();
+
+    total_time -= for_time;
+    printf("\tTotalTime=%dus, AverageTime=%.3fus (%.3fcycles)\n", total_time, total_time/float(iterations), total_time/float(iterations)*CLOCK);
+
+    return total_time;
+}
+
+int test_exp(int iterations) {
+    int for_time, total_time;
+    volatile float a, b, c;
+    volatile int i;
+
+    pc.printf("Testing exp()...");
+    /* Determine loop overhead */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+    }
+    for_time=timer.read_us();
+    timer.stop();
+
+    a = 1.7;
+    b = 2.2;
+    /* Determine the total loop time */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+        c=exp(a);
+    }
+    total_time=timer.read_us();
+    timer.stop();
+
+    total_time -= for_time;
+    printf("\tTotalTime=%dus, AverageTime=%.3fus (%.3fcycles)\n", total_time, total_time/float(iterations), total_time/float(iterations)*CLOCK);
+
+    return total_time;
+}
+
+int test_log(int iterations) {
+    int for_time, total_time;
+    volatile float a, b, c;
+    volatile int i;
+
+    pc.printf("Testing log()...");
+    /* Determine loop overhead */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+    }
+    for_time=timer.read_us();
+    timer.stop();
+
+    a = 1.7;
+    b = 2.2;
+    /* Determine the total loop time */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+        c=log(a);
+    }
+    total_time=timer.read_us();
+    timer.stop();
+
+    total_time -= for_time;
+    printf("\tTotalTime=%dus, AverageTime=%.3fus (%.3fcycles)\n", total_time, total_time/float(iterations), total_time/float(iterations)*CLOCK);
+
+    return total_time;
+}
+
+float data[100];
+int test_std(int iterations) {
+    int for_time, total_time;
+    volatile float a, b, c;
+    volatile int i;
+
+    for(int n = 0; n < sizeof(data) / sizeof(data[0]); n++) {
+        data[n] = 10 + n * 0.01;
+    }
+    pc.printf("Testing std(%d)...", sizeof(data)/sizeof(data[0]));
+    /* Determine loop overhead */
+    timer.reset();
+    timer.start();
+    for (i=0; i<iterations; i++) {
+    }
     for_time=timer.read_us();
     timer.stop();
 
     /* Determine the total loop time */
     timer.reset();
     timer.start();
-
-    /* The operation takes place in the body of
-    this for loop. */
-
-    for (i=0; i<ITERATIONS; i++) {
-        c=a+b;
-        // c=sin(a);
-        // c=sqrt(a);
-        // c = log(b);
-        // c = exp(a);
+    for (i=0; i<iterations; i++) {
+        a = 0;
+        b = 0;
+        for(int n = 0; n < sizeof(data) / sizeof(data[0]); n++) {
+            a += data[n];
+        }
+        a /= sizeof(data) / sizeof(data[0]);
+        for(int n = 0; n < sizeof(data) / sizeof(data[0]); n++) {
+            c = data[n] - a;
+            b += c * c;
+        }
+        b /= sizeof(data) / sizeof(data[0]);
+        b = sqrt(b);
     }
     total_time=timer.read_us();
+    timer.stop();
 
-    operation_time = total_time-for_time;   // Calculate the time it took for the number of operations
+    total_time -= for_time;
+    printf("\tTotalTime=%dus, AverageTime=%.3fus (%.3fcycles)\n", total_time, total_time/float(iterations), total_time/float(iterations)*CLOCK);
 
-    single_operation_time=float(operation_time)/float(ITERATIONS);
-    number_of_cycles = single_operation_time*CLOCK;
+    return total_time;
+}
 
-    pc.printf("for overhead: \t\t%dus \n", for_time);
-    pc.printf("total time: \t\t%dus \n\n", total_time);
-    pc.printf("%d calculations took:\t%dus \n", ITERATIONS, operation_time);
-    pc.printf("single operation took: \t\t%fus\n", single_operation_time);
-    pc.printf("single operation took: \t\t%.3f cycles\n", number_of_cycles);
+int main() {
+    test_plus(ITERATIONS*10);
+    test_sub(ITERATIONS*10);
+    test_mul(ITERATIONS*10);
+    test_div(ITERATIONS*10);
+    test_sqrt(ITERATIONS);
+    test_sin(ITERATIONS);
+    test_exp(ITERATIONS);
+    test_log(ITERATIONS);
+    test_std(ITERATIONS/10);
 }
